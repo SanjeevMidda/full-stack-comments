bb9314144f5da15e8def58982df5a23046f382f6
Sanjeev Midda <110365686+SanjeevMidda@users.noreply.github.com>
2024-03-17 23:03:40 +0000

    Add files via upload
    Uploading front end of application.

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./index.css";
+
+function App() {
+  const [input, setInput] = useState("");
+
+  console.log(input);
+
+  return (
+    <div className="App">
+      <div className="inputContainer">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <h3>></h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
